38f6916043163a8919f26d0aa0ceb114513b5ecf
Earl Cameron <mr.e.cameron@gmail.com>
2021-07-22 00:25:11 -0400

    increment and decrement thrugh settings range

--- controls.py
+++ controls.py
@@ -3,17 +3,23 @@ def cameraControls(pygame, event, menuPos, menus):
         # print(event.key)
         if event.key == pygame.K_UP:
             # print("up")
-            if menuPos[menuPos[3]] >= 1:
-                menuPos[menuPos[3]] = menuPos[menuPos[3]] - 1
+            if menuOptionType(menuPos, menus) == "list":
+                if menuPos[menuPos[3]] >= 1:
+                    menuPos[menuPos[3]] = menuPos[menuPos[3]] - 1
+            elif menuOptionType(menuPos, menus) == "range":
+                menuOptionUpdateValue(1, menuPos, menus)
 
         elif event.key == pygame.K_DOWN:
             # print("down")
-            print("menupos-->", menuPos, " | menu lenght-->",
-                  menuLimits(menuPos, menus))
-            if menuPos[menuPos[3]] >= menuLimits(menuPos, menus)-1:
-                menuPos[menuPos[3]] = 0
-            else:
-                menuPos[menuPos[3]] = menuPos[menuPos[3]] + 1
+            # print("menupos-->", menuPos, " | menu lenght-->",
+            #       menuLimits(menuPos, menus))
+            if menuOptionType(menuPos, menus) == "list":
+                if menuPos[menuPos[3]] >= menuLimits(menuPos, menus)-1:
+                    menuPos[menuPos[3]] = 0
+                else:
+                    menuPos[menuPos[3]] = menuPos[menuPos[3]] + 1
+            elif menuOptionType(menuPos, menus) == "range":
+                menuOptionUpdateValue(-1, menuPos, menus)
 
         elif event.key == pygame.K_RIGHT:
             # print("right")
@@ -47,3 +53,20 @@ def menuLimits(menuPosArr, menus):
     except:
         print("Out of range")
     return 0
+
+
+def menuOptionType(menuPosArr, menus):
+    return menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["type"]
+
+
+def menuOptionUpdateValue(direction, menuPosArr, menus):
+    if direction > 0:
+        if menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["value"] < menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["max"]:
+            menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["value"] = \
+                menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["value"] +\
+                    menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["step"]
+    else:
+        if menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["value"] > menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["min"]:
+            menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["value"] = \
+                menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["value"] -\
+                    menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"]["step"]
