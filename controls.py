17338710d729fa22997ee9b624befbf4f75dd5d6
Earl Cameron <mr.e.cameron@gmai.com>
2021-07-23 01:36:11 -0400

    corrected navigation logic simplified menulimits fn

--- controls.py
+++ controls.py
@@ -1,7 +1,8 @@
-def cameraControls(pygame, event, menuPos, menus):
+def cameraControls(pygame, event, menuPos, menus, camera=None):
     if event.type == pygame.KEYDOWN:
         # print(event.key)
-
+        print(
+            f"pos: {menuPos} || menu len: {menuLimits(menuPos, menus)} || type: {menuOptionType(menuPos, menus)}")
         if event.key == pygame.K_UP:
             # print("up")
             if menuOptionType(menuPos, menus) == "list":
@@ -28,27 +29,22 @@ def cameraControls(pygame, event, menuPos, menus):
 def menuLimits(menuPosArr, menus):
     # print(menus)
     try:
-        if "menus" in menus:
-            # print(menus["menus"])
-            if "options" in menus["menus"][menuPosArr[0]]:
-                # print(menus["menus"][menuPosArr[0]]["options"])
-                if menuPosArr[3] == 0:
-                    return len(menus["menus"][menuPosArr[0]]["options"])
-                if "options" in menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]:
-                    # print(menus["menus"][menuPosArr[0]]["options"])
-                    if menuPosArr[3] == 1:
-                        return len(menus["menus"][menuPosArr[0]]["options"])
-                    if "options" in menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]:
-                        # print(len(menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"])-1)
-                        if menuPosArr[3] == 2:
-                            return len(menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"])
+        if menuPosArr[3] == 0:
+            return len(menus["menus"])
+        elif menuPosArr[3] == 1:
+            return len(menus["menus"][menuPosArr[1]]["options"])
+        elif menuPosArr[3] == 2:
+            return len(menus["menus"][menuPosArr[1]]["options"][menuPosArr[2]]["options"])
     except:
         print("Out of range")
     return 0
 
 
 def menuOptionType(menuPosArr, menus):
-    return menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["type"]
+    if menuPosArr[3] == 1:
+        return menus["menus"][menuPosArr[0]]["type"]
+    else:
+        return menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["type"]
 
 
 def menuOptionSelector(direction, menuPosArr):
@@ -80,13 +76,17 @@ def menuOptionUpdateListValue(direction, menuPosArr, menus):
     if direction > 0:
         if menuPosArr[menuPosArr[3]] >= 1:
             menuPosArr[menuPosArr[3]] = menuPosArr[menuPosArr[3]] - 1
+        print('over here')
     else:
         if menuPosArr[menuPosArr[3]] >= menuLimits(menuPosArr, menus)-1:
             menuPosArr[menuPosArr[3]] = 0
+            print('here')
         else:
             menuPosArr[menuPosArr[3]] = menuPosArr[menuPosArr[3]] + 1
-        
+            print('there')
+
     # update value based on menu position
     if menuPosArr[2] > 0:
         menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["value"] = \
-            menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]]["options"][menuPosArr[2]]
+            menus["menus"][menuPosArr[0]]["options"][menuPosArr[1]
+                                                     ]["options"][menuPosArr[2]]
